c432a5051b0b7e88b3c18ca0bc718afbf78ea22a
chibuisi <enyia21@gmail.com>
2020-10-27 10:51:58 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+class DigitalClicker extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            timesClicked: 0
+        }
+    }
+    
+    clicked= () => {
+        this.setState({
+            timesClicked: this.state.timesClicked + 1
+        }, ()=>console.log(this.state.timesClicked) );
+    }
+    render(){
+        return(
+        <button onClick={this.clicked}>{this.state.timesClicked}</button>
+        )
+    }
+
+}
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,48 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+        }
+    }
+
+    updateBitrate = () =>{ 
+        this.setState({
+            settings:{
+                ...this.state.settings,
+                bitrate: 12
+            }
+        }, () => console.log(this.state))
+    }
+
+    updateResolution = () =>{
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    resolution: '720p'
+                }
+            }
+        })
+    }
+
+    render(){
+        return(
+            <div className="update">
+                <br /><button className="bitrate" onClick={this.updateBitrate}>Bitrate: {this.state.settings.bitrate}</button>
+        <br /><button className="resolution" onClick={this.updateResolution}>Resolution: {this.state.settings.video.resolution}</button>
+            </div>
+        )
+    }
+}
+export default YouTubeDebugger;
